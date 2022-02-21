86d50daafc0115fb642dd25834f5601eeade11df
annaklem <84881356+annaklem@users.noreply.github.com>
2022-02-21 13:03:13 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let btnChat = document.getElementsByClassName("chat");
+
+// let travellers = document.getElementById("travelers");
+
+
+btnChat.addEventListener('click', openChat)
+function openChat(){
+    
+
+    travellers.classList.add("travelers_open")
+    // btnChat.innerHTML = "YOu cliked me";
+}
+
